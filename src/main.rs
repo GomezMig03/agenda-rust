e7dcc96d20b59ee4dd0ba033f719b636e6fd07fc
gomezmig03 <gomezmig03@gmail.com>
2024-10-08 19:57:22 +0200

    Add record writer

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
+use core::str;
 use std::io::{self, stdin, Write};
 use std::fs::{File, OpenOptions};
+use text_io::read;
 
 static AGENDA: &str = "./agenda.txt";
 
-fn main() {
-    main_loop();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    main_loop()?;
+    Ok(())
 }
 
 
-fn main_loop() {
+fn main_loop() -> io::Result<()> {
     let mut user_input: String = String::from("1");
     while user_input != "0" {
         println!("Select option:");
@@ -17,17 +20,18 @@ fn main_loop() {
         println!("2. Edit record");
         println!("3. Add record");
         println!("4. Delete record");
-        stdin().read_line(&mut user_input).expect("Error reading user input.");
+        user_input = read!();
 
         match user_input.as_str() {
-            "0" => return,
+            "0" => return Ok(()),
             "1" => todo!(),
             "2" => todo!(),
-            "3" => todo!(),
+            "3" => write_record()?,
             "4" => todo!(),
-            _ => println!("Select a valid option.")
+            _ => println!("Select a valid option."),
         }
     }
+    Ok(())
 }
 
 fn get_file() -> io::Result<File> {
@@ -38,13 +42,47 @@ fn get_file() -> io::Result<File> {
         .open(AGENDA)
 }
 
-fn write_record(record: &str) -> io::Result<()>{
+fn write_record() -> io::Result<()>{
     let mut file = get_file()?;
+    let record = write_handler();
 
     writeln!(file, "{}", record)?;
     Ok(())
 }
 
-fn write_handler() {
-    todo!()
+fn write_handler() -> String {
+    let mut event = String::from("Default-name");
+    let desc = String::from("test");
+
+    let mut day: u8;
+    let mut month: u8;
+    let mut year: u32;
+    print!("Write an event name: ");
+    event = read!();
+
+    while {
+        print!("\nWrite the day of the event: ");
+        day = read!();
+        day >= 31
+    } {}
+
+    while {
+        print!("\nWrite the month of the event: ");
+        month = read!();
+        month >= 12
+    } {}
+
+    while {
+        print!("\nWrite the year of the event: ");
+        year = read!();
+        year <= 2024
+    } {}
+
+    let date = write_formatter(day, month, year);
+
+    format!("{};{};{},", event, date, desc)
+}
+
+fn write_formatter(day: u8, month: u8, year: u32) -> String {
+    format!("{}-{}-{}", day, month, year)
 }
